09ec125d555303978309d85768b7aaf1020a001d
WanjiruMargaret <maggiesamwells@gmail.com>
2025-07-27 10:32:31 +0300

    test passed

---
+++ src/components/NavBar.js
@@ -0,0 +1,31 @@
+import { NavLink } from "react-router-dom";
+import "../components/NavBar.css"; // Assuming you have a CSS file for styling the NavBar
+
+/* define the NavBar component */
+function NavBar() {
+  return (
+    <nav>
+      <NavLink
+        to="/"
+        /* add styling to Navlink */
+        className="nav-link"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        className="nav-link"
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="/login"
+        className="nav-link"
+      >
+        Login
+      </NavLink>
+    </nav>
+  );
+};
+
+export default NavBar;
